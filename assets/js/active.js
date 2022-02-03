daf7e99f12f0e2bdb95ca50c705eeff720cfa532
dilshad-shibin <msdcva786@gmail.com>
2022-02-03 14:12:35 +0530

    new edits

--- assets/js/active.js
+++ assets/js/active.js
@@ -1,20 +1,3 @@
-// Index of jQuery Active Code
-
-// :: 1.0 PRELOADER ACTIVE CODE
-// :: 2.0 NAVIGATION MENU ACTIVE CODE
-// :: 3.0 SCROLL TO TOP ACTIVE CODE
-// :: 4.0 SCROLL LINK ACTIVE CODE
-// :: 5.0 SMOOTH SCROLLING ACTIVE CODE
-// :: 6.0 PORTFOLIO ACTIVE CODE
-// :: 7.0 AOS ACTIVE CODE
-// :: 8.0 PREVENT DEFAULT ACTIVE CODE
-// :: 9.0 FEATURES SLIDER ACTIVE CODE
-// :: 10.0 BRANDING SLIDER ACTIVE CODE
-// :: 11.0 TESTIMONIALS ACTIVE CODE
-// :: 12.0 TEAM SLIDER ACTIVE CODE
-// :: 13.0 APP SCREENSHOTS ACTIVE CODE
-// :: 14.0 FANCYBOX IMAGE GALLERY
-// :: 15.0 CONTACT FORM ACTIVE CODE
 
 (function ($) {
     'use strict';
@@ -110,29 +93,6 @@
         }, 1000);
     });
 
-    // :: 6.0 PORTFOLIO ACTIVE CODE
-    $('#container').imagesLoaded( function() {
-      $('.portfolio').isotope({
-          // set itemSelector so .grid-sizer is not used in layout
-          itemSelector: '.grid',
-          percentPosition: true,
-          masonry: {
-            // use outer width of grid-sizer for columnWidth
-            columnWidth: '.grid'
-          }
-        });
-      // filter items on button click
-        $('.portfolio-menu').on( 'click', 'button', function() {
-          var filterValue = $(this).attr('data-filter');
-          $('.portfolio').isotope({ filter: filterValue });
-        });
-
-        $('.portfolio-menu button').on( 'click', function() {
-            $(this).siblings('.active').removeClass('active');
-            $(this).addClass('active');
-        });
-    });
-
     // :: 7.0 AOS ACTIVE CODE
     AOS.init();
 
@@ -141,44 +101,7 @@
         $.preventDefault();
     });
 
-    // :: 9.0 FEATURES SLIDER ACTIVE CODE
-    $('.features-slider.owl-carousel').owlCarousel({
-        items: 1,
-        loop: true,
-        nav: false,
-        dots: true,
-        smartSpeed: 1500,
-        autoplay: true,
-        autoplayTimeout: 6000,
-        dotsContainer: '.features-content'
-    });
-
-    // :: 10.0 BRANDING SLIDER ACTIVE CODE
-    $('.branding-slider.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 20,
-        nav: false,
-        dots: false,
-        smartSpeed: 2000,
-        autoplay: true,
-        autoplayTimeout: 4000,
-        responsive: {
-            0: {
-                items: 2
-            },
-            576: {
-                items: 3
-            },
-            768: {
-                items: 4
-            },
-            992: {
-                items: 5
-            }
-        }
-    });
-
-    // :: 11.0 TESTIMONIALS ACTIVE CODE
+   // :: 11.0 TESTIMONIALS ACTIVE CODE
     $('.testi-slider.owl-carousel').owlCarousel({
         loop: true,
         margin: 0,
@@ -231,7 +154,7 @@
     //Feature Slider
     $('.feature-slider.owl-carousel').owlCarousel({
         loop: true,
-        margin: 20,
+        margin: 0,
         nav: false,
         dots: true,
         smartSpeed: 2000,
@@ -239,10 +162,10 @@
         autoplayTimeout: 4000,
         responsive: {
             0: {
-                items: 2
+                items: 3
             },
             576: {
-                items: 2
+                items: 3
             },
             768: {
                 items: 3
@@ -251,134 +174,6 @@
                 items: 3
             }
         }
-    });
-
-    // :: 12.0 TEAM SLIDER ACTIVE CODE
-    $('.team-slider.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 20,
-        nav: false,
-        dots: false,
-        smartSpeed: 2000,
-        autoplay: true,
-        autoplayTimeout: 4000,
-        responsive: {
-            0: {
-                items: 1,
-                nav: false
-            },
-            576: {
-                items: 2
-            },
-            768: {
-                items: 3
-            },
-            992: {
-                items: 4
-            }
-        }
-    });
-
-    // :: 13.0 APP SCREENSHOTS ACTIVE CODE
-    $('.app-screenshots').slick({
-        dots: true,
-        arrows: false,
-        speed: 2000,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        pauseOnHover: false,
-        pauseOnFocus: false,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    dots: false
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    dots: false
-                }
-            }
-          ]
-    });
-
-    // :: 14.0 FANCYBOX IMAGE GALLERY
-    $('[data-fancybox="images"]').fancybox({
-        afterLoad: function (instance, current) {
-            var pixelRatio = window.devicePixelRatio || 1;
-
-            if (pixelRatio > 1.5) {
-                current.width = current.width / pixelRatio;
-                current.height = current.height / pixelRatio;
-            }
-        }
-    });
-
-    $('[data-fancybox]').fancybox({
-        youtube: {
-            controls: 0,
-            showinfo: 0
-        },
-        vimeo: {
-            color: 'f00'
-        }
-    });
-
-    // :: 15.0 CONTACT FORM ACTIVE CODE
-    // Get the form.
-    var form = $('#contact-form');
-    // Get the messages div.
-    var formMessages = $('.form-message');
-    // Set up an event listener for the contact form.
-    $(form).submit(function (e) {
-        // Stop the browser from submitting the form.
-        e.preventDefault();
-        // Serialize the form data.
-        var formData = $(form).serialize();
-        // Submit the form using AJAX.
-        $.ajax({
-                type: 'POST',
-                url: $(form).attr('action'),
-                data: formData
-            })
-            .done(function (response) {
-                // Make sure that the formMessages div has the 'success' class.
-                $(formMessages).removeClass('error');
-                $(formMessages).addClass('success');
-
-                // Set the message text.
-                $(formMessages).text(response);
-
-                // Clear the form.
-                $('#contact-form input,#contact-form textarea').val('');
-            })
-            .fail(function (data) {
-                // Make sure that the formMessages div has the 'error' class.
-                $(formMessages).removeClass('success');
-                $(formMessages).addClass('error');
-
-                // Set the message text.
-                if (data.responseText !== '') {
-                    $(formMessages).text(data.responseText);
-                } else {
-                    $(formMessages).text('Oops! An error occured and your message could not be sent.');
-                }
-            });
     });
 
 }(jQuery));
