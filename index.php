46be5f7ba585b2a730633d27fad854bc8b9ff989
dilshad-shibin <msdcva786@gmail.com>
2022-02-11 10:19:51 +0530

    favicon added

--- index.php
+++ index.php
@@ -2,14 +2,9 @@
 <html class="no-js" lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="description" content="">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Title  -->
     <title>Seaoux Doors</title>
     <?php include('header/head.php'); ?>
-
 </head>
 
 <body class="homepage-1">
